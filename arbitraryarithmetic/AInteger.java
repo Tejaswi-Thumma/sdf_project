c407e3869681a60510c128086e662ba4a9eea289
Tejaswi-Thumma <tejaswithumma123@gmail.com>
2025-05-01 23:45:57 +0530

    Removing leading zeros function

--- arbitraryarithmetic/AInteger.java
+++ arbitraryarithmetic/AInteger.java
@@ -23,3 +23,30 @@ public class AInteger {
     public static AInteger parse(String s) {
         return new AInteger(s);
     }
+
+    //removing leading zeros and keeping the sign same
+    public static String removing_leading_zeros ( String s) {
+        boolean isNegative = false;
+        String s2 = s;
+
+        if(s.startsWith("-")) {
+            isNegative = true;
+            s2 = s2.substring(1);
+            
+        }
+        String duplicate_s2 = s2;
+        for(int i=0;i< duplicate_s2.length()-1;i++) {
+            if(s2.charAt(0) == '0') {
+                s2 = s2.substring(1);
+            }
+            else {
+                break;
+            }
+        }
+        if(isNegative && !s2.equals("0")) {
+            return "-" + s2;
+        }
+        else {
+            return s2;
+        }
+    }
